e0bf253ea289ad209ec75204788debc626cf3fc1
Christoph Schuerz <christoph.schuerz@boku.ac.at>
2016-06-05 17:44:26 +0200

    adding comments to Rscripts of session 3

--- Session3/RScripts/1_ggplots_previous_examples.R
+++ Session3/RScripts/1_ggplots_previous_examples.R
@@ -1,18 +1,57 @@
-# Plotting overview of bio data -------------------------------------------
-bio_data %<>% mutate(Replication = as.factor(Replication))
+#In the following examples I explain the plots I presented already in the
+#previous sessions. For the boxplots from session 1 I also tried a similar plot
+#with the base barplot() function.
 
+# Plotting overview of bio data -------------------------------------------
+# Again we define Replication as factor variable
+bio_data <- mutate(bio_data, Replication = as.factor(Replication))
+
+# Every ggplot starts with the command ggplot() further layers and controls for 
+# labels, legends etc. are added with a "+". In this example I defined the data
+# I want to work with already in the ggplot() command and the aesthetics (x, y,
+# etc.) for the individual layers. I could have also defined the data in the
+# individual layers, but also define the aesthetics in the ggplot() command, as
+# illustrated in further examples.
 ggplot(data = bio_data) + 
+  # For plotting a violinplot I added the layer violin(), on the x axis I plot
+  # the variable water and on the y axis the Gain. I fill the boxes with gray
+  # color.
   geom_violin(aes(x = Water, y = Gain_TDM),fill = "grey80") + 
+  # Over the violin plot I plot a boxplot with the same aesthetics (Therfore I
+  # could have added them already in the ggplot() command and ommit them in the
+  # layers.). As I want the boxes to be very thin I set the width to 0.1
   geom_boxplot(aes(x = Water, y = Gain_TDM), width = 0.1) + 
+  # I want to seperate the plots for the individual varieties. This is done with
+  # a facet command (in this case facet_wrap) and the information for which
+  # variable to separate. I also added the number of columns I want to arrange
+  # the plots.
   facet_wrap(~Variety, ncol = 2)
 
+
+# The very same information content can be illustrated in a different way. Here
+# I added all varieties into one plot window and seperated for each variety the
+# plots by their fill colors.
+
+# Again start with a ggplot() command.
 ggplot(bio_data) + 
+  # I again start with a violin layer but in this case x is Variety and the fill
+  # color is controlled by the variable Water. Please see that fill is now 
+  # defined inside the aes() command and not outside as in the previous example.
+  # To control the distance between the violins of WS and WW I use the position
+  # command.
   geom_violin(aes(x = Variety, y = Gain_TDM, fill = Water),
               position=position_dodge(0.5)) + 
+  # The same I repeat for the boxplot layer.
   geom_boxplot(aes(x = Variety, y = Gain_TDM, position = Water), 
                position=position_dodge(0.5), width = 0.1)
 
-# Example with classical boxplot() function
+
+
+# Example with classical boxplot() function. This example illustrates that a
+# similar plot as in the first example done with the basic plot commands can be
+# rather tidious. This does not mean that basic plot functions are in general
+# worse than ggplot(), they have some advantages in other situations. But I
+# prefer ggplot in most situations.
 par(mfrow = c(3,3))
 boxplot(Gain_TDM~Water, data = bio_data[bio_data$Variety == "Caesar",],
         ylab = "Gain_TDM", main = "Caesar", ylim = c(10,35))
@@ -30,10 +69,22 @@ boxplot(Gain_TDM~Water, data = bio_data[bio_data$Variety == "Spunta",],
         ylab = "Gain_TDM", main = "Spunta", ylim = c(10,35))
 
 # Plotting results of water content data ----------------------------------
-ggplot(data = SD2C) +
-  geom_line(aes(x = date, y = sim)) + 
-  geom_pointrange(aes(x = date, y = obs_mean, ymin = obs_lw, ymax = obs_up)) + 
-  geom_point(aes(x = date, y = grv), col = "coral3", pch = 15) +
+
+# Again start with a ggplot() command. As date is plotted on the x axis for all
+# three layers I added it directly to the ggplot command.
+ggplot(data = SD2C, aes(x = date)) +
+  # The first layer I add is a line plot for the simulated time series. So the
+  # aesthetic y is sim.
+  geom_line(aes(y = sim)) +
+  # As next layer I add the water content measurements for which the statistics
+  # mean, and standard error were calculated. To visualize mean and errors in
+  # the plot I used the geometry pointrange, where I set y, ymin and ymax
+  geom_pointrange(aes(y = obs_mean, ymin = obs_lw, ymax = obs_up), col = "cadetblue") +
+  # For the gravimetric measurements single values per timestep and depth are
+  # available. I added them as points to the plots.
+  geom_point(aes(y = grv), col = "coral3", pch = 15) +
+  # Again I wanted to seperate the plots for the individual depths for the
+  # measurements. As done before I apply a facet command for the variable depth
   facet_wrap(~depth, ncol = 4) 
 
 
--- Session3/RScripts/2_ggplots_different_plot_types.R
+++ Session3/RScripts/2_ggplots_different_plot_types.R
@@ -1,26 +1,67 @@
 # Different plot types ----------------------------------------------------
 
-# Histograms
+# In the following examples I want to demonstrate different ggplot geometries to
+# present different types of variables (eg. one continuous variable, one 
+# descrete and one continuous, etc.). When easily possible I also tried to get a
+# similar example with the basic plot commands.
+
+
+# One continuous variable ------------------------------------------------- 
+# Histograms Histograms are a good way to illustrate the distribution for single
+# continuous variables. As in the previous examples the ggplot starts with a
+# ggplot() command.
 ggplot() + 
-  geom_histogram(data = bio_data, aes(Gain_TDM), bins = 10) + 
+  # The geometry to use is geom_histogram(). As only one variable is
+  # illustrated, the aesthetics only require a x Variable. I distributed the
+  # data over 10 bins (10 classes).
+  geom_histogram(data = bio_data, aes(x = Gain_TDM), bins = 10) + 
+  # When now separating the plots for the variable Water you can see that the
+  # number of bins is defined for the data first and applied to the two plots
+  # the same way. This gives you a good comparability of the two subsets of the
+  # data.
   facet_wrap(~Water)
 
+
+# The basic plot command for a histogram is hist(). To do the separation for the
+# variable Water I need to separate the plot window first with the par() command
+# and set the option of mfrow to 1 row and 2 columns.
 par(mfrow = c(1,2))
+# After that I plot the histograms for the two subsets Water == "WS" and Water == "WW".
 hist(bio_data$Gain_TDM[bio_data$Water == "WS"], 
      main = "WS", xlab = "Gain_TDM", breaks = 10)
 hist(bio_data$Gain_TDM[bio_data$Water == "WW"], 
      main = "WW", xlab = "Gain_TDM", breaks = 10)
+# Here you see that the bins are individual for both plots and the data is
+# difficult to compare. In order to achieve the same result you would have to
+# set the breaks for the classes individually.
+
+# To close the plot window with the separation into two subplots I close the plotting device with the command dev.off()
 dev.off()
 
-# Point plots
-ggplot(data = bio_data) + 
-  geom_point(aes(x = Total_T, y = Gain_TDM, 
-                 col = Variety, size = Water, 
+# Two continuous variables ------------------------------------------------ 
+# Point plots In this example I use two continuous variables from the bio data
+# set. What I want to point out in this example are the possibilities to control
+# the information content of a plot by using different aesthetics such as x,y,
+# col, size, shape, or aplha (hue of the data point). (Nevertheless the plot
+# here makes sense or not).
+ggplot(data = bio_data, aes(x = Total_T, y = Gain_TDM)) + 
+  geom_point(aes(col = Variety, size = Water, 
                  alpha = Total_T, shape = Replication)) + 
-  geom_smooth(aes(x = Total_T, y = Gain_TDM),method = "lm") +
+  # I also fitted a linear model to the data set with the command geom_smooth.
+  geom_smooth(method = "lm") +
+  # Again I sepearated into individual plots for the different varieties.
   facet_wrap(~Variety, ncol = 2)
 
+# The basic plot command for point data in R is plot()
 plot(bio_data$Total_T, bio_data$Gain_TDM, xlab = "Total_T", ylab = "Gain_TDM")
+
+
+
+# x is descrete, y is continuous ------------------------------------------
+
+# In your discipline data is often given as continuous properties (e.g. weight,
+# length) for different categories (e.g. crop varieties, treatments). A very
+# common way to illustrate such information is a barplot.
 
 # Barplots with errorbars
 bio_summary <- bio_data %>% 
